ef19bafb65f82f3b1a3269f10fdc7dc882b59ff7
xgfone <xgfone@126.com>
2018-12-10 14:48:29 +0800

    add the middleware ResetResponse

---
+++ middleware/reset_response.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/xgfone/ship"
+)
+
+// ResetResponse wraps and reset the response.
+func ResetResponse(reset func(http.ResponseWriter) http.ResponseWriter) ship.Middleware {
+	return func(next ship.Handler) ship.Handler {
+		return func(ctx ship.Context) error {
+			if resp := reset(ctx.Response()); resp != nil {
+				ctx.SetResponse(resp)
+			}
+			return next(ctx)
+		}
+	}
+}
