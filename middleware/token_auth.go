7b9c1dbf3da359b6c6ac1cc6caacb9b7b0decebe
xgfone <xgfone@126.com>
2018-12-10 17:49:08 +0800

    add token auth middleware

---
+++ middleware/token_auth.go
@@ -0,0 +1,90 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/xgfone/ship"
+)
+
+// TokenAuth returns a TokenAuth middleware.
+//
+// For valid key it will calls the next handler.
+// For invalid key, it responds "401 Unauthorized".
+// For missing key, it responds "400 Bad Request".
+//
+// If getAuthTokenFromRequest is missing, the default is
+// GetAuthTokenFromHeader(ship.HeaderAuthorization, "auth").
+func TokenAuth(validateToken func(token string) (bool, error),
+	getAuthTokenFromRequest ...func(ctx ship.Context) (token string, err error)) ship.Middleware {
+	getAuthToken := GetAuthTokenFromHeader(ship.HeaderAuthorization, "auth")
+	if len(getAuthTokenFromRequest) > 0 && getAuthTokenFromRequest[0] != nil {
+		getAuthToken = getAuthTokenFromRequest[0]
+	}
+
+	return func(next ship.Handler) ship.Handler {
+		return func(ctx ship.Context) error {
+			token, err := getAuthToken(ctx)
+			if err != nil {
+				return ship.NewHTTPError(http.StatusBadRequest, err.Error())
+			}
+			if valid, err := validateToken(token); err != nil {
+				return err
+			} else if valid {
+				return next(ctx)
+			}
+			return ship.ErrUnauthorized
+		}
+	}
+}
+
+// GetAuthTokenFromHeader is used to get the CSRF token from the request header.
+func GetAuthTokenFromHeader(header string, authScheme ...string) func(ship.Context) (string, error) {
+	var scheme string
+	if header == ship.HeaderAuthorization {
+		if len(authScheme) > 0 {
+			scheme = strings.TrimSpace(authScheme[0])
+		}
+		if scheme == "" {
+			panic(errors.New("ship: TokenAuth requires authScheme for Authorization"))
+		}
+	}
+	schemelen := len(scheme)
+
+	return func(ctx ship.Context) (string, error) {
+		auth := ctx.Request().Header.Get(header)
+		if auth == "" {
+			return "", errors.New("missing auth token in request header")
+		}
+		if schemelen > 0 {
+			if len(auth) > schemelen+1 && auth[:schemelen] == scheme {
+				return auth[schemelen+1:], nil
+			}
+			return "", errors.New("invalid auth token in the request header")
+		}
+		return ctx.Request().Header.Get(header), nil
+	}
+}
+
+// GetAuthTokenFromForm is used to get the CSRF token from the request body FORM.
+func GetAuthTokenFromForm(param string) func(ship.Context) (string, error) {
+	return func(ctx ship.Context) (string, error) {
+		token := ctx.FormValue(param)
+		if token != "" {
+			return token, nil
+		}
+		return "", errors.New("missing auth token in the form parameter")
+	}
+}
+
+// GetAuthTokenFromQuery is used to get the CSRF token from the request URL query.
+func GetAuthTokenFromQuery(param string) func(ship.Context) (string, error) {
+	return func(ctx ship.Context) (string, error) {
+		token := ctx.QueryParam(param)
+		if token != "" {
+			return token, nil
+		}
+		return "", errors.New("missing auth token in the url query")
+	}
+}
---
+++ middleware/token_auth_test.go
@@ -0,0 +1,76 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/xgfone/ship"
+)
+
+func TestAuthToken(t *testing.T) {
+	assert := assert.New(t)
+	s := ship.New()
+
+	validateToken := func(token string) (bool, error) {
+		return token == "valid_token", nil
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	ctx := s.NewContext(req, rec)
+	authMiddleware := TokenAuth(validateToken, GetAuthTokenFromHeader(ship.HeaderAuthorization, "abc"))
+	handler := authMiddleware(func(ctx ship.Context) error {
+		return ctx.String(http.StatusOK, "test")
+	})
+
+	// Valid AuthToken
+	auth := "abc valid_token"
+	req.Header.Set(ship.HeaderAuthorization, auth)
+	assert.NoError(handler(ctx))
+
+	// Invalid AuthToken
+	auth = "abc invalid_token"
+	req.Header.Set(ship.HeaderAuthorization, auth)
+	he := handler(ctx).(ship.HTTPError)
+	assert.Equal(http.StatusUnauthorized, he.Code())
+
+	// Missing Authorization header
+	req.Header.Del(ship.HeaderAuthorization)
+	he = handler(ctx).(ship.HTTPError)
+	assert.Equal(http.StatusBadRequest, he.Code())
+
+	// Token from custom header
+	handler = TokenAuth(validateToken, GetAuthTokenFromHeader("API-Token"))(
+		func(ctx ship.Context) error {
+			return ctx.String(http.StatusOK, "test")
+		})
+	req.Header.Set("API-Token", "valid_token")
+	assert.NoError(handler(ctx))
+
+	// Token from URL query
+	handler = TokenAuth(validateToken, GetAuthTokenFromQuery("token"))(
+		func(ctx ship.Context) error {
+			return ctx.String(http.StatusOK, "test")
+		})
+	query := req.URL.Query()
+	query.Add("token", "valid_token")
+	req.URL.RawQuery = query.Encode()
+	assert.NoError(handler(ctx))
+
+	// Token from Form
+	handler = TokenAuth(validateToken, GetAuthTokenFromForm("token"))(
+		func(ctx ship.Context) error {
+			return ctx.String(http.StatusOK, "test")
+		})
+	form := make(url.Values)
+	form.Set("token", "valid_token")
+	req = httptest.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+	req.Header.Set(ship.HeaderContentType, ship.MIMEApplicationForm)
+	ctx = s.NewContext(req, rec)
+	assert.NoError(handler(ctx))
+}
