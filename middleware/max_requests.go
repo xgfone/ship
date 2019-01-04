ad4e3c4b6fe54161dfc46648b841fc4a5c68f342
xgfone <xgfone@126.com>
2019-01-04 14:29:25 +0800

    the max requests middleware

---
+++ middleware/max_requests.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"net/http"
+	"sync/atomic"
+
+	"github.com/xgfone/ship"
+)
+
+// MaxRequests returns a Middleware to allow the maximum number of the requests
+// to max at a time.
+//
+// If the number of the requests exceeds the maximum, it will call the handler,
+// which return the status code 429. But you can appoint yourself handler.
+func MaxRequests(max uint32, handler ...ship.Handler) ship.Middleware {
+	h := func(c ship.Context) error { return c.NoContent(http.StatusTooManyRequests) }
+	if len(handler) > 0 && handler[0] != nil {
+		h = handler[0]
+	}
+
+	var maxNum = int32(max)
+	var current int32
+
+	return func(next ship.Handler) ship.Handler {
+		return func(ctx ship.Context) error {
+			atomic.AddInt32(&current, 1)
+			defer atomic.AddInt32(&current, -1)
+
+			if atomic.LoadInt32(&current) > maxNum {
+				return h(ctx)
+			}
+			return next(ctx)
+		}
+	}
+}
---
+++ middleware/max_requests_test.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/xgfone/ship"
+)
+
+func TestMaxRequests(t *testing.T) {
+	sleep := time.Millisecond
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	s := ship.New()
+	s.Use(MaxRequests(2, func(c ship.Context) error {
+		c.NoContent(http.StatusTooManyRequests)
+		wg.Done()
+		return nil
+	}))
+	s.R("/").GET(func(ctx ship.Context) error {
+		time.Sleep(sleep)
+		wg.Done()
+		return nil
+	})
+
+	req1 := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec1 := httptest.NewRecorder()
+	req2 := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec2 := httptest.NewRecorder()
+	req3 := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec3 := httptest.NewRecorder()
+
+	go s.ServeHTTP(rec1, req1)
+	go s.ServeHTTP(rec2, req2)
+	go s.ServeHTTP(rec3, req3)
+
+	wg.Wait()
+	time.Sleep(sleep)
+	if rec1.Code+rec2.Code+rec3.Code != 200+200+429 {
+		t.Errorf("req1=%d, req2=%d, req3=%d", rec1.Code, rec2.Code, rec3.Code)
+	}
+}
